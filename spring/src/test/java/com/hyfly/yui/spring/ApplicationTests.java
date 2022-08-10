803cf86133ea6ebdc8e201620381005b3ed79c39
hyfly233 <1274694743@qq.com>
2022-08-10 20:41:37 +0800

    commit

--- spring/src/test/java/com/hyfly/yui/spring/ApplicationTests.java
+++ spring/src/test/java/com/hyfly/yui/spring/ApplicationTests.java
@@ -1,9 +1,13 @@
 package com.hyfly.yui.spring;
 
 import com.alibaba.fastjson2.JSONObject;
+import com.hyfly.yui.spring.domain.TestUserPo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
@@ -24,10 +28,26 @@ class ApplicationTests {
     }
 
     @Test
+    void testLocalhost() {
+        String url = "http://192.168.0.102:8080";
+        String result = restTemplate.getForObject(url, String.class);
+        System.out.println(result);
+    }
+
+
+    @Test
     void testToJsonField() {
         TestUserPo po = TestUserPo.builder().id("1").name("hyfly").password("123456").build();
 
-        System.out.println(JSONObject.toJSONString(po));
+        String jsonData = JSONObject.toJSONString(po);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<>(jsonData, httpHeaders);
+
+        String s = restTemplate.postForObject("http://192.168.0.102:18000/json", entity, String.class);
+        System.out.println(s);
     }
 
 }
