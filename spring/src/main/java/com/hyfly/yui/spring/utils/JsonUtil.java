657d1b636c9d887e3a1de014d47499f2c0623b68
hyfly233 <1274694743@qq.com>
2022-08-10 18:54:12 +0800

    commit

---
+++ spring/src/main/java/com/hyfly/yui/spring/utils/JsonUtil.java
@@ -0,0 +1,8 @@
+package com.hyfly.yui.spring.utils;
+
+public class JsonUtil {
+
+    public String toJson(Object object) {
+        return null;
+    }
+}
--- spring/src/test/java/com/hyfly/yui/spring/ApplicationTests.java
+++ spring/src/test/java/com/hyfly/yui/spring/ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.hyfly.yui.spring;
 
+import com.alibaba.fastjson2.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,6 +21,13 @@ class ApplicationTests {
         String url = "https://www.baidu.com";
         String result = restTemplate.getForObject(url, String.class);
         System.out.println(result);
+    }
+
+    @Test
+    void testToJsonField() {
+        TestUserPo po = TestUserPo.builder().id("1").name("hyfly").password("123456").build();
+
+        System.out.println(JSONObject.toJSONString(po));
     }
 
 }
